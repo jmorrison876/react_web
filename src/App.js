b5055b36a2e8a005bf758ded91213e51a8e9a0cc
jmorrison876 <jason_utech@hotmail.com>
2024-05-01 20:46:57 -0400

    changed display message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MUST  NICE!
+         Now we are rocking!
         </p>
         <a
           className="App-link"
